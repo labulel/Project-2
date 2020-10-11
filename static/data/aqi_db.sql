933b4240a59e99e0a97657b8a04627e4f32b782c
L L <labulel@hotmail.com>
2020-10-11 15:13:47 -0400

    added AQI db

---
+++ static/data/aqi_db.sql
@@ -0,0 +1,90 @@
+-- CREATE TABLE AQI_2020 (
+-- 	State_Name VARCHAR(255),
+-- 	county_name VARCHAR(255),
+-- 	State_Code INT NOT NULL,
+-- 	County_Code INT NOT NULL,
+-- 	Date DATE NOT NULL,
+-- 	AQI INT NOT NULL,
+-- 	Category VARCHAR(255),
+-- 	Defining_Parameter VARCHAR(255),
+-- 	Defining_Site VARCHAR(255),
+-- 	Number_of_Sites_Reporting INT NOT NULL);
+	
+
+-- CREATE TABLE AQI_2015 (
+-- 	State_Name VARCHAR(255),
+-- 	county_name VARCHAR(255),
+-- 	State_Code INT NOT NULL,
+-- 	County_Code INT NOT NULL,
+-- 	Date DATE NOT NULL,
+-- 	AQI INT NOT NULL,
+-- 	Category VARCHAR(255),
+-- 	Defining_Parameter VARCHAR(255),
+-- 	Defining_Site VARCHAR(255),
+-- 	Number_of_Sites_Reporting INT NOT NULL);
+
+-- select * from aqi_2015;
+
+
+-- CREATE TABLE us_state_abbr (
+--  	State VARCHAR(255),
+-- 	Abbreviation VARCHAR(255)
+-- );
+
+-- select * from us_state_abbr;
+
+-- select * from us_county_coordinates;
+
+-- INSERT INTO us_state_abbr (state, abbreviation)
+-- values ('Alabama','AL'),
+-- ('Alaska','AK'),
+-- ('Arizona','AZ'),
+-- ('Arkansas','AR'),
+-- ('California','CA'),
+-- ('Colorado','CO'),
+-- ('Connecticut','CT'),
+-- ('Delaware','DE'),
+-- ('District of Columbia','DC'),
+-- ('Florida','FL'),
+-- ('Georgia',	'GA'),
+-- ('Hawaii','HI'),
+-- ('Idaho','ID'),
+-- ('Illinois','IL'),
+-- ('Indiana',	'IN'),
+-- ('Iowa','IA'),
+-- ('Kansas','KS'),
+-- ('Kentucky','KY'),
+-- ('Louisiana','LA'),
+-- ('Maine','ME'),
+-- ('Montana','MT'),
+-- ('Nebraska','NE'),
+-- ('Nevada','NV'),
+-- ('New Hampshire','NH'),
+-- ('New Jersey','NJ'),
+-- ('New Mexico','NM'),
+-- ('New York','NY'),
+-- ('North Carolina','NC'),
+-- ('North Dakota','ND'),
+-- ('Ohio','OH'),
+-- ('Oklahoma','OK'),
+-- ('Oregon', 'OR'),
+-- ('Maryland','MD'),
+-- ('Massachusetts','MA'),
+-- ('Michigan','MI'),
+-- ('Minnesota','MN'),
+-- ('Mississippi','MS'),
+-- ('Missouri','MO'),
+-- ('Pennsylvania','PA'),
+-- ('Rhode Island','RI'),
+-- ('South Carolina','SC'),
+-- ('South Dakota','SD'),
+-- ('Tennessee','TN'),
+-- ('Texas','TX'),
+-- ('Utah','UT'),
+-- ('Vermont','VT'),
+-- ('Virginia','VA'),
+-- ('Washington','WA'),
+-- ('West Virginia','WV'),
+-- ('Wisconsin','WI'),
+-- ('Wyoming','WY');
+
